ab90d858ceb84ba44ef3c2f96499232bf57cac19
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-08 21:19:05 +0600

    dynamic data fetching done

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+const getSinglePost = async (post_id) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${post_id}`
+  );
+  const data = await res.json();
+  return data;
+};
+
+const SinglePost = async ({ params }) => {
+  const p = await params;
+  const singlePost = await getSinglePost(p.id);
+  return (
+    <div className="h-screen place-content-center place-items-center space-y-4">
+      {/* Single Post {JSON.stringify(singlePost)} */}
+      <p className="font-bold text-lg text-red-600">Id: {singlePost.id}</p>
+      <p className="text-yellow-600">User Id: {singlePost.userId}</p>
+      <p className="font-semibold text-blue-600">Title: {singlePost.title}</p>
+      <p className="text-gray-400">{singlePost.body}</p>
+      <Link href={"/posts"}>Go Back</Link>
+    </div>
+  );
+};
+
+export default SinglePost;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const getPosts = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
@@ -10,14 +12,23 @@ const Posts = async () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8">
       {posts.map((post) => (
         <div
-          key={post.id}
-          className="border border-green-700 p-6 rounded-lg shadow-lg"
-        >
-          <p className="font-bold text-lg text-red-600">Id: {post.id}</p>
-          <p className="text-yellow-600">User Id: {post.userId}</p>
-          <p className="font-semibold text-blue-600">Title: {post.title}</p>
+        key={post.id}
+        className="border border-green-700 p-6 rounded-lg shadow-lg flex flex-col"
+      >
+        <p className="font-bold text-lg text-red-600">Id: {post.id}</p>
+        <p className="text-yellow-600">User Id: {post.userId}</p>
+        <p className="font-semibold text-blue-600">Title: {post.title}</p>
+      
+        {/* Make this div grow to push the Details link down */}
+        <div className="flex-grow">
           <p className="text-gray-400">{post.body}</p>
         </div>
+      
+        <Link href={`/posts/${post.id}`} className="text-blue-500 underline">
+          Details
+        </Link>
+      </div>
+      
       ))}
       {/* <p>{JSON.stringify(posts)}</p> */}
     </div>
