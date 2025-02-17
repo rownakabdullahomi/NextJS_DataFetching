bd64461af5ff4d1f72b50ec3632f4b8229e29b0b
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-17 12:58:11 +0600

    next js styling

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import style from "./post.module.css";
 
 export const getPosts = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -17,11 +18,11 @@ const Posts = async () => {
       >
         <p className="font-bold text-lg text-red-600">Id: {post.id}</p>
         <p className="text-yellow-600">User Id: {post.userId}</p>
-        <p className="font-semibold text-blue-600">Title: {post.title}</p>
+        <p className={`font-semibold text-blue-600 ${style["post-title"]}`}>Title: {post.title}</p>
       
         {/* Make this div grow to push the Details link down */}
         <div className="flex-grow">
-          <p className="text-gray-400">{post.body}</p>
+          <p className="text-gray-400 testing-purpose">{post.body}</p>
         </div>
       
         <Link href={`/posts/${post.id}`} className="text-blue-500 underline">
